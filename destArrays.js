2afef3c2d1c049c9731cc77e692eb73008448bfb
Yan Kolev <y.kolev11@gmail.com>
2024-04-02 17:32:46 +0300

    Destructuring arrays practice

--- destArrays.js
+++ destArrays.js
@@ -181,3 +181,53 @@ const books = [
       highlighted: true
     }
   ];
+
+  /* TASKS:
+  1.Destructure the books array into two variables called firstBook and secondBook.
+  
+  2.Destructure the books array into a variable called thirdBook. You must skip the first two books.
+
+  3. Below is the nested ratings array that contains two other arrays. Destructure the nested ratings arrays into two variables called rating and ratingsCount.
+   In the result of your destructuring, 
+   the ratings variable should store a number 4.19, and the ratingsCount variable should store a number 144584.
+
+  const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+  4.Below is the ratingStars array. Destructure it into three variables called fiveStarRatings, oneStarRatings and threeStarRatings. Assign the threeStarRatings variable with a default value of 0.
+
+    const ratingStars = [63405, 1808]; */
+
+
+// const firstBook = books [0];
+// const secondBook = books [1];
+// console.log(firstBook, secondBook);
+
+//1
+const [firstBook , secondBook] = books
+
+//2
+const [, , thirdBook] = books
+console.log(thirdBook);
+
+//3
+
+ //nested arrays destructuring
+
+//  const nested = [2,4, [5,6]];
+//  const [i, ,j] = nested;
+//  console.log(i, j);
+
+// const[ i, , [j, k]] = nested;
+// console.log(i, j, k );
+
+const ratings = [['rating', 4.19], ['ratingsCount', 144584]];
+
+const [[, rating], [, ratingsCount]] = ratings;
+console.log (ratings, ratingsCount);
+
+//4
+
+const ratingStars = [63405, 1808];
+
+
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
