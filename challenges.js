992c4960521bab53c956f13a915f872c5aea82db
Yan Kolev <y.kolev11@gmail.com>
2024-04-19 16:58:47 +0300

    solution to challenges coursewise

--- challenges.js
+++ challenges.js
@@ -138,3 +138,24 @@ Hummels: 1,
 Lewandowski: 2
 }
 */
+
+//1)
+for (const [i, player] of game.scored.entries())
+console.log(`Goal ${ i + 1}: ${player}`);
+
+//2)
+const odds = Object.values(game.odds)
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+//3)
+for (const[team, odd] of Object.entries(game.odds)){
+    const teamStr = team === x ? 'draw' : `victory' ${game[team]}`
+    console.log(`Odd of ${teamStr} ${odd}`);
+}
+
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
