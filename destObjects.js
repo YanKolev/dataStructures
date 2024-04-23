867ed7e0d5c5fe87d05da862cd61aa3cf4fd1ab5
Yan Kolev <y.kolev11@gmail.com>
2024-04-23 15:55:43 +0300

    Learning about JS Maps

--- destObjects.js
+++ destObjects.js
@@ -424,5 +424,33 @@ const staff = ['Waiter','Chef', 'Waiter', 'Manager','Chef','Waiter'];
 const staffUnique = [...new Set(staff)]; //with spread operator we can create array from the set
 
 
+// ----MAPS----
 
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy')
+rest.set(2, 'Lisbon, Portugal');
+
+
+//set chaining
+rest.set('categories',['Italian', 'Pizzeria', 'Vegetarian', 'Organic']
+.set('open',11)
+.set('close',23) 
+.set(true, 'We are open') 
+.set(false, 'We are closed'));
+
+const time = 21;
+console.log(rest.get(time >rest.get('open')&& time < rest.get('close')))
+
+console.log (rest.has('categories')); //deleting information from the map
+
+rest.delete(2)
+
+console.log(rest.size);
+//rest.clear- clearing the map 
+//you can uses arrays or objects as map keys
+
+rest.set([1,2], 'Test');
+rest.set(document.querySelector('h1'), 'Heading')
+console.log(rest.get([1,2]))
