39e18181df3d6f8e0f65ac759d47bd50533e4490
Yan Kolev <y.kolev11@gmail.com>
2024-04-24 21:30:17 +0300

    Solutions to challenges

--- challenges.js
+++ challenges.js
@@ -195,3 +195,24 @@ const gameEvents = new Map([
     [80, '⚽ GOAL'],
     [92, '🔶 Yellow card'],
     ]);
+
+//1)
+
+const events = [...new Set (gameEvents.values())];
+console.log(events)
+
+//2)
+gameEvenets.delete(64);
+
+//3)
+console.log(`An event happened on average, every ${90 / gameEvents.size} minutes`)
+const time = [...gameEvents.keys()].pop();
+
+console.log(`An event happened on average, every ${time / gameEvents.size} minutes`)
+
+//4)
+
+for (const[key, value] of gameEvents){
+    const half = min <= 45 ? 'FIRST': 'SECOND'
+    console.log(`[${half} HALF] ${min}: ${event}`)
+}
